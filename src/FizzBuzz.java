5a5daffb1343b96077155bd8bb2c4284d3a7f551
sam198 <jcn3509@gmail.com>
2024-09-13 17:43:58 -0400

    001

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i < 100) {
+        while (i <= 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
